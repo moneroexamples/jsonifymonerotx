6d4c5b86cfc54330e1f8d9ec9c34f75f5beaa2ad
moneroexamples <moneroexamples@tuta.io>
2018-12-06 13:02:15 +0800

    README.md updated

--- README.md
+++ README.md
@@ -1,7 +1,55 @@
 ## Makes complete json objects of transactions and blocks
 
 Useful for producing json of txs and blocks for use in unit tests
-in other projects
+in other projects. The will be used primarly in openmonero and the onion explorer
+for generating transaction and block json representations for unit tests.
+
+## Example compilation on Ubuntu 18.04
+
+C++14 is required to run this code.
+
+#### Monero download and compilation
+
+Download and compile recent Monero into your home folder:
+
+```bash
+# first install monero dependecines
+sudo apt update
+
+sudo apt install git build-essential cmake libboost-all-dev miniupnpc libunbound-dev graphviz doxygen libunwind8-dev pkg-config libssl-dev libcurl4-openssl-dev libgtest-dev libreadline-dev libzmq3-dev libsodium-dev libhidapi-dev libhidapi-libusb0
+
+# go to home folder
+cd ~
+
+git clone --recurse-submodules https://github.com/monero-project/monero
+
+cd monero/
+
+USE_SINGLE_BUILDDIR=1 make
+```
+
+
+#### Compilation of the xmregcore
+
+```bash
+
+# go to home folder if still in ~/monero
+cd ~
+
+git clone --recurse-submodules  https://github.com/moneroexamples/jsonifymonerotx.git
+
+cd jsonifymonerotx
+
+mkdir build && cd build
+
+cmake ..
+
+# altearnatively can use cmake -DMONERO_DIR=/path/to/monero_folder ..
+# if monero is not in ~/monero
+
+make
+
+```
 
 ## Other examples
 
--- main.cpp
+++ main.cpp
@@ -1,54 +1,105 @@
-#include "src/om_log.h"
 #include "src/MicroCore.h"
 #include "src/tools.h"
+
+#include "boost/program_options.hpp"
+#include <boost/filesystem.hpp>
 
 #include <iostream>
 #include <memory>
 
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 using namespace std;
 using namespace cryptonote;
 
 int
-main(int ac, const char* av[])
+main(int argc, const char *argv[])
 {
+
+    network_type nettype = network_type::STAGENET;
+
+    string blockchain_path = xmreg::get_default_lmdb_folder(nettype);
+
+    string hash_str;
+    string sender_str;
+
+    fs::path blckchain_path;
+
+    try
+    {
+        po::options_description desc("jsonifymonerotx: construct complete json representaiton of monero tx or blk");
+
+        desc.add_options()
+            ("help,h", "Help")
+            ("nettype,n", po::value<size_t>()->default_value(static_cast<size_t>(nettype)),
+             "network type: 0-MAINNET, 1-TESTNET, 2-STAGENET")
+            ("blockchain-path,b", po::value<string>()->default_value(blockchain_path),
+             "Path to lmdb folder containing the blockchain")
+            ("sender,s", po::value<string>(),
+            "Optinal sender's address,viewkey,spendkey");
+
+        po::positional_options_description pos_desc;
+        pos_desc.add("hash", -1);
+
+        po::command_line_parser parser{argc, argv};
+        parser.options(desc).positional(pos_desc).allow_unregistered();
+        po::parsed_options parsed_options = parser.run();
+
+        po::variables_map vm;
+        store(parsed_options, vm);
+        notify(vm);
+
+        if (vm.count("help"))
+        {
+            cout << desc << '\n';
+            return EXIT_SUCCESS;
+        }
+
+        if (vm.count("blockchain-path"))
+            blckchain_path = fs::path {vm["blockchain-path"].as<string>()};
+
+        if (vm.count("hash"))
+            hash_str = vm["hash"].as<string>();
+
+        if (vm.count("sender"))
+            sender_str = vm["sender"].as<string>();
+
+//        if (vm.count("anglestep"))
+//            angle_step = std::min(std::max(vm["anglestep"].as<size_t>(), static_cast<size_t>(1)), static_cast<size_t>(45));
+
+//        if (vm.count("savegray"))
+//            grayscale_dir = fs::path {vm["savegray"].as<string>()};
+
+//        if (vm.count("csvfilename"))
+//            csvfname = vm["csvfilename"].as<string>();
+
+//        if (vm.count("roseplotsdir"))
+//            roseplotsdir = vm["roseplotsdir"].as<string>();
+
+//        if (save_roseplots == false)
+//            roseplotsdir = "";
+    }
+    catch (const po::error &ex)
+    {
+        std::cerr << ex.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
 
 // setup monero logger
 mlog_configure(mlog_get_default_log_path(""), true);
 mlog_set_log("1");
 
-el::Configurations defaultConf;
 
-defaultConf.setToDefault();
+cout << "Blockchain path: " << blockchain_path << '\n';
+cout << "Initializaing MicroCore\n";
 
-defaultConf.setGlobally(el::ConfigurationType::ToStandardOutput, "true");
-
-el::Loggers::reconfigureLogger("jsonifyxmrtx", defaultConf);
-
-OMINFO << "Program is starting";
-
-network_type nettype = network_type::STAGENET;
+xmreg::MicroCore mcore {blockchain_path, nettype};
 
 
-string blockchain_path = xmreg::get_default_lmdb_folder(nettype);
 
-OMINFO << "Blockchain path: " << blockchain_path;
-
-
-// create blockchainsetup instance and set its parameters
-// such as blockchain status monitoring thread parameters
-
-// once we have all the parameters for the blockchain and our backend
-// we can create and instance of CurrentBlockchainStatus class.
-// we are going to do this through a shared pointer. This way we will
-// have only once instance of this class, which we can easly inject
-// and pass around other class which need to access blockchain data
-
-xmreg::MicroCore mcore;
-
-OMINFO << "Initializaing MicroCore";
-mcore.init(blockchain_path, nettype);
-
+cout << "Program finished.\n";
 
 return EXIT_SUCCESS;
 }
