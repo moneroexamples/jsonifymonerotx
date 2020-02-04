d411a3a1f75f75d345e9db49230d790633307f8e
moneroexamples <moneroexamples@tuta.io>
2020-02-04 17:24:09 +0800

    add main to gtest

--- tests/foundobjectprocessor_tests.cpp
+++ tests/foundobjectprocessor_tests.cpp
@@ -171,4 +171,21 @@ TEST(SUBADDRESS, HasSubaddressIndex)
 
 }
 
+
 }
+
+int main(int argc, char **argv)
+{
+    
+    ::testing::InitGoogleTest(&argc, argv);
+
+    //from https://github.com/google/googletest/issues/765#issuecomment-215663692
+    for (int i = 1; i < argc; ++i) 
+    {
+         printf("arg %2d = %s\n", i, argv[i]);
+    }
+
+    return RUN_ALL_TESTS();
+}
+
+
